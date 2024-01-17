c215a1738cd779fd646ff525701b0b86dc42c5cf
YKTian-x2b <2084984251@qq.com>
2024-01-17 11:30:19 +0800

    mod train_mp

--- Matformer-main/matformer/scripts/mp/train_mp.py
+++ Matformer-main/matformer/scripts/mp/train_mp.py
@@ -7,4 +7,9 @@ props = [
     "shear modulus",
     # "elastic anisotropy",
 ]
-train_prop_model(learning_rate=0.001,name="matformer", dataset="megnet", prop=props[0], pyg_input=True, n_epochs=500, batch_size=64, use_lattice=True, output_dir="./matformer_mp_formation", use_angle=False, save_dataloader=False)
+# train_prop_model(learning_rate=0.001,name="matformer", dataset="megnet", prop=props[0], pyg_input=True, n_epochs=500, batch_size=64, use_lattice=True, output_dir="./matformer_mp_formation", use_angle=False, save_dataloader=False)
+
+train_prop_model(learning_rate=0.001,name="matformer", dataset="megnet", prop=props[2], pyg_input=True,
+                 n_epochs=500, batch_size=64, use_lattice=True, output_dir="./matformer_mp_bulk",
+                 use_angle=False, save_dataloader=False, mp_id_list="bulk")
+
