013a83f12a8544e0da664e3bd9ce8c4c18620b62
YKTian-x2b <2084984251@qq.com>
2024-04-24 22:45:50 +0800

    update CGCNN_paddle complete code

--- Paddle/CGCNN_paddle/usePaddle.py
+++ Paddle/CGCNN_paddle/usePaddle.py
@@ -1,15 +1,44 @@
-import paddle
-from paddle import nn
+# import paddle
+# from paddle import nn
+import numpy as np
+import os
+from pymatgen.core.structure import Structure
 
 if __name__ == "__main__":
-    N = 2
-    M = 3
-    hidden_fea_len = 4
-    # atoms_in_fea: (N, atom_fea_len）
-    # atom_bonds_in_fea: (N, M, bond_fea_len)
-    # atom_nbrs_idx: (N, M)
+    root_dir = './root_dir'
+    cif_id = 'mp-7090'
+    crystal = Structure.from_file(os.path.join(root_dir, cif_id + '.cif'))
+    # print(crystal)
+    # print(len(crystal))
+    # for i in range(len(crystal)):
+    #     print(crystal[i])
 
-    atoms_in_fea = paddle.to_tensor([[0.1, 0.3, 0.2, 0.4], [0.5, 0.6, 0.8, 0.7]])
-    atom_nbrs_idx = paddle.to_tensor([[0, 1, 0], [1, 1, 0]])
-    atom_nbr_fea = atoms_in_fea[atom_nbrs_idx, :]
-    print(atom_nbr_fea.shape)
+    all_nbrs = crystal.get_all_neighbors(8, include_index=True)
+    # all_nbrs = [sorted(nbrs, key=lambda x: x[1]) for nbrs in all_nbrs]
+    # nbr_fea_idx, nbr_fea = [], []
+    # for nbr in all_nbrs:
+    #     if len(nbr) < 12:
+    #         nbr_fea_idx.append(list(map(lambda x: x[2], nbr)) +
+    #                            [0] * (12 - len(nbr)))
+    #         nbr_fea.append(list(map(lambda x: x[1], nbr)) +
+    #                        [8 + 1.] * (12 - len(nbr)))
+    #     else:
+    #         nbr_fea_idx.append(list(map(lambda x: x[2],
+    #                                     nbr[:12])))
+    #         nbr_fea.append(list(map(lambda x: x[1],
+    #                                 nbr[:12])))
+    #
+    # print(nbr_fea_idx)
+    # print(nbr_fea)
+    print(all_nbrs[0][0])
+
+    for jjj in range(len(all_nbrs[0][0])):
+        print(all_nbrs[0][0][jjj])
+    # dmin = 0
+    # dmax = 8
+    # step = 0.2
+    # filter = np.arange(dmin, dmax + step, step)
+    # var = step
+    # distances = []
+    #
+    # np.exp(-(distances[..., np.newaxis] - filter) ** 2 / var ** 2)
