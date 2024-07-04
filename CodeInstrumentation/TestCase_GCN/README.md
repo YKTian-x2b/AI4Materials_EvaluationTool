4b4c61298f8aca8a91c3bf8b1ca1c26fb3829f9e
YKTian-x2b <2084984251@qq.com>
2024-07-05 00:19:08 +0800

    GCN_torch & GCN_paddle

--- CodeInstrumentation/TestCase_GCN/README.md
+++ CodeInstrumentation/TestCase_GCN/README.md
@@ -2,4 +2,4 @@
 
 材料学 GCN模型 插桩分析
 
-# 遇到包不对的问题 就 pip --force-reinstall
+### 遇到包不对的问题 就 pip --force-reinstall
--- CodeInstrumentation/TestCase_GCN/profile.sh
+++ CodeInstrumentation/TestCase_GCN/profile.sh
@@ -1,8 +1,7 @@
  #!/bin/bash
 
-nsys profile --stats=true -f true -o res/gcn_profile_nsys \
-python main.py root_dir > res/gcn_profile_nsys.txt
+nsys profile --stats=true -f true -o res/PaddleRes/paddle_gcn_profile_nsys \
+python GCN_paddle/main.py root_dir > res/PaddleRes/paddle_gcn_profile_nsys.txt
 
-
-
-# python main.py root_dir
+nsys profile --stats=true -f true -o res/TorchRes/torch_gcn_profile_nsys \
+python GCN_torch/predict.py GCN_torch/model_best.pth.tar root_dir > res/TorchRes/torch_gcn_profile_nsys.txt
